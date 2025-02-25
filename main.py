80d9a951169394744ab9a5721b32e192f834bc78
Brando Debroy <2818292920101@ingenieria.usac.edu.gt>
2025-02-24 22:38:08 -0600

    corrección

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import flask
-import requests
 
-def create_app():
-    app = flask.Flask(__name__)
+app = flask.Flask(__name__)
 
-    @app.route('/check')
-    def check():
-        JsonStructure = {
-            "Code": 200,
-            "Status": "OK"
-        }
-        return JsonStructure
-    
-    @app.route('/info')
-    def info():
-        JsonStructure = {
-            "Instancia": "1",
-            "Curso": "Seminario de sistemas 1 A",
-            "Grupo": "9",
-        }
-        return JsonStructure
+@app.route('/check')
+def check():
+    JsonStructure = {
+        "Code": 200,
+        "Status": "OK"
+    }
+    return JsonStructure
 
-    return app
+@app.route('/info')
+def info():
+    JsonStructure = {
+        "Instancia": "1",
+        "Curso": "Seminario de sistemas 1 A",
+        "Grupo": "9",
+    }
+    return JsonStructure
+
+if __name__ == "__main__":
+    app.run(debug=True, port=3000)
